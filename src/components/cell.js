426e476671771d47c9f96e005fd65b964f6bfca1
Vitor Leal <vitor_leal2201@hotmail.com>
2020-10-01 00:28:21 -0300

    Add initial corner and street positions
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- src/components/cell.js
+++ src/components/cell.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-function Cell() {
-  return <div style={{width: 100, height: 100 , backgroundColor:'red'}}/>;
+function Cell({...props}) {
+  return <div style={{width: 100, height: 100 , backgroundColor: props.color}}/>;
 }
 
 export default Cell;
--- src/views/Phase1/index.js
+++ src/views/Phase1/index.js
@@ -3,20 +3,42 @@ import Cell from '../../components/cell';
 
 const Phase1 = () => {
   const createMatrix = () => {
-    let width = Math.round(window.innerWidth / 100);
-    let height = Math.round(window.innerHeight / 100);
-    let widthArray = Array.from(Array(width).keys());
-    let heightArray = Array.from(Array(height).keys());
+    let width = Math.floor(window.innerWidth / 100);
+    let height = Math.floor(window.innerHeight / 105);
+    let matrix = [];
+    let withNoHouse = [{ x: 0, y: 0 }, { x: 1, y: 1 }, 
+      { x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }, { x: 5, y: 5 }, { x: 1, y: 0 }, { x: 2, y: 0 },
+     { x: 3, y: 0 }, { x: 4, y: 0 }, { x: 5, y: 0 }, { x: 6, y: 0 }];
+    for(let i=0; i < width; i++){
+      let aux = [];
+      for(let j=0; j < height;j++){
+        if (withNoHouse.find(item => item.x === i && item.y === j)){
+          aux.push({home: true});
+        } else {
+          aux.push({home: false});
+        }
+      }
+      matrix.push(aux);
+    }
+
     return (
       <div style={{ width: '100%', display: 'flex', height: '95%' }}>
         {
-          widthArray.map((row, i) => (
+          matrix.map((item, i) => 
             <div key={i}>
-              {heightArray.map((col, j) => (
-                <Cell />
-              ))}
+              {
+                item.map((props,j) => {
+                  if(props.home){
+                    return <Cell color="white"/>
+                  } else if(props.corner) {
+                    return <Cell color = "orange"/>
+                  } else {
+                    return <Cell color="black"/>
+                  }
+                })
+              }
             </div>
-          ))
+          )
         }
       </div>
     )
