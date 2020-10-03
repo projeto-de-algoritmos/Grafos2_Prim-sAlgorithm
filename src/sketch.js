0262f7231de42a236737fc512d5832fe72836296
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-10-02 23:08:13 -0300

    Finally details
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/sketch.js
+++ src/sketch.js
@@ -15,40 +15,38 @@ function mousePressed() {
 }
 
 function draw() {
-  background(0);
+  background(50);
 
   if (init) {
     var reached = [];
     var unreached = [];
 
-    for (var i = 0; i < vertices.length; i++) {
-      unreached.push(vertices[i]);
-    }
+    unreached = [...vertices];
 
     reached.push(unreached[0]);
     unreached.splice(0, 1);
 
     while (unreached.length > 0) {
-      var record = 100000;
+      var gDistance = 100000;
       var rIndex;
       var uIndex;
-
-      for (var i = 0; i < reached.length; i++) {
-        for (var j = 0; j < unreached.length; j++) {
-          var v1 = reached[i];
-          var v2 = unreached[j];
+      
+      reached.map((reachedNode , i) => {
+        unreached.map((unreachedNode, j) => {
+          var v1 = reachedNode;
+          var v2 = unreachedNode;
           var d = dist(v1.x, v1.y, v2.x, v2.y);
 
-          if (d < record) {
-            record = d;
+          if (d < gDistance) {
+            gDistance = d;
             rIndex = i;
             uIndex = j;
           }
-        }
-      }
+        })
+      })
 
       stroke(255);
-      strokeWeight(2);
+      strokeWeight(3);
       line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
       reached.push(unreached[uIndex]);
       unreached.splice(uIndex, 1);
@@ -57,7 +55,7 @@ function draw() {
     for (let i = 0; i < vertices.length; i++) {
       fill(255);
       stroke(255);
-      ellipse(vertices[i].x, vertices[i].y, 16, 16)
+      ellipse(vertices[i].x, vertices[i].y, 30, 30)
     }
   } else {
     renderInitGame();
