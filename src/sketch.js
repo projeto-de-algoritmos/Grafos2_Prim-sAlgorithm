c48964238228988dad5cf2308011c2c3aa62ace4
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-10-02 22:43:40 -0300

    Adding initial menu explaining how to works
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/sketch.js
+++ src/sketch.js
@@ -1,56 +1,79 @@
 const vertices = [];
+let init = false;
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function mousePressed() {
-  var v = createVector(mouseX, mouseY);
-  vertices.push(v);
+  if (init) {
+    var v = createVector(mouseX, mouseY);
+    vertices.push(v);
+  } else {
+    init = true;
+  }
 }
 
 function draw() {
-  background(51);
+  background(0);
 
-  var reached = [];
-  var unreached = [];
+  if (init) {
+    var reached = [];
+    var unreached = [];
 
-  for (var i = 0; i < vertices.length; i++) {
-    unreached.push(vertices[i]);
-  }
-
-  reached.push(unreached[0]);
-  unreached.splice(0, 1);
-
-  while (unreached.length > 0) {
-    var record = 100000;
-    var rIndex;
-    var uIndex;
-
-    for (var i = 0; i < reached.length; i++) {
-      for (var j = 0; j < unreached.length; j++) {
-        var v1 = reached[i];
-        var v2 = unreached[j];
-        var d = dist(v1.x, v1.y, v2.x, v2.y);
-
-        if ( d < record) {
-          record = d;
-          rIndex = i;
-          uIndex = j;
-        }
-      }
+    for (var i = 0; i < vertices.length; i++) {
+      unreached.push(vertices[i]);
     }
 
-    stroke(255);
-    strokeWeight(2);
-    line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
-    reached.push(unreached[uIndex]);
-    unreached.splice(uIndex, 1);
-  }
+    reached.push(unreached[0]);
+    unreached.splice(0, 1);
 
-  for (let i = 0; i < vertices.length; i++) {
-    fill(255);
-    stroke(255);
-    ellipse(vertices[i].x, vertices[i].y, 16, 16)
+    while (unreached.length > 0) {
+      var record = 100000;
+      var rIndex;
+      var uIndex;
+
+      for (var i = 0; i < reached.length; i++) {
+        for (var j = 0; j < unreached.length; j++) {
+          var v1 = reached[i];
+          var v2 = unreached[j];
+          var d = dist(v1.x, v1.y, v2.x, v2.y);
+
+          if (d < record) {
+            record = d;
+            rIndex = i;
+            uIndex = j;
+          }
+        }
+      }
+
+      stroke(255);
+      strokeWeight(2);
+      line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
+      reached.push(unreached[uIndex]);
+      unreached.splice(uIndex, 1);
+    }
+
+    for (let i = 0; i < vertices.length; i++) {
+      fill(255);
+      stroke(255);
+      ellipse(vertices[i].x, vertices[i].y, 16, 16)
+    }
+  } else {
+    renderInitGame();
   }
+}
+
+function renderInitGame() {
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth * 0.02);
+  fill(255);
+  text("Prim's algorithm", windowWidth / 2, windowHeight / 8);
+  textSize(windowWidth * 0.015);
+  text("Here you can dynamically see how the prim algorithm works, have fun :)", windowWidth * 0.5, windowHeight * 0.2);
+  text("To add nodes in the graph, just click on the position of your new node!", windowWidth * 0.5, windowHeight * 0.3);
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth * 0.01);
+  fill(255);
+  text('Click to continue...', windowWidth / 2, windowHeight * 0.9);
 }
