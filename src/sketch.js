9a6f0a571587fdab2dbe8cf0b01472fbafc2bbf6
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-10-02 22:27:50 -0300

    Implemented Prim's algorithm successfully
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,50 @@
-let vertices = [];
+const vertices = [];
 
 function setup() {
   createCanvas(640, 360);
 }
 
 function mousePressed() {
-  let v = createVector(mouseX, mouseY);
+  var v = createVector(mouseX, mouseY);
   vertices.push(v);
 }
 
 function draw() {
   background(51);
+
+  var reached = [];
+  var unreached = [];
+
+  for (var i = 0; i < vertices.length; i++) {
+    unreached.push(vertices[i]);
+  }
+
+  reached.push(unreached[0]);
+  unreached.splice(0, 1);
+
+  while (unreached.length > 0) {
+    var record = 100000;
+    var rIndex;
+    var uIndex;
+
+    for (var i = 0; i < reached.length; i++) {
+      for (var j = 0; j < unreached.length; j++) {
+        var v1 = reached[i];
+        var v2 = unreached[j];
+        var d = dist(v1.x, v1.y, v2.x, v2.y);
+
+        if ( d < record) {
+          record = d;
+          rIndex = i;
+          uIndex = j;
+        }
+      }
+    }
+
+    line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
+    reached.push(unreached[uIndex]);
+    unreached.splice(uIndex, 1);
+  }
 
   for (let i = 0; i < vertices.length; i++) {
     fill(255);
